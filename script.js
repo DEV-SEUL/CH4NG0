069a32945334d70f970ba47f7d5ba7fefa027cc2
DEVSEUL <SEULCOC7@GMAIL.COM>
2025-06-12 23:38:43 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,8 @@
-body {
-  margin: 0;
-  padding: 0;
-  background-image: url('https://i.imgur.com/O4Q8RKw.jpeg'); /* 배경 이미지 */
-  background-size: cover;
-  background-position: center;
-  font-family: 'Courier New', monospace;
-  color: #fff;
-  text-align: center;
-}
-
-.container {
-  margin-top: 20%;
-  background-color: rgba(0, 0, 0, 0.5);
-  display: inline-block;
-  padding: 2em;
-  border-radius: 10px;
-}
-
-button {
-  margin-top: 1em;
-  padding: 10px 20px;
-  background: #f0c040;
-  border: none;
-  font-weight: bold;
-  cursor: pointer;
-}
+// 예: 커서 따라다니는 원
+document.addEventListener('mousemove', (e) => {
+  const circle = document.querySelector('.cursor');
+  if (circle) {
+    circle.style.left = `${e.pageX}px`;
+    circle.style.top = `${e.pageY}px`;
+  }
+});
