b4baa3e154c6a54620457af214bb6c3715d3b029
DEVSEUL <SEULCOC7@GMAIL.COM>
2025-06-12 23:43:42 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,1 @@
-// 예: 커서 따라다니는 원
-document.addEventListener('mousemove', (e) => {
-  const circle = document.querySelector('.cursor');
-  if (circle) {
-    circle.style.left = `${e.pageX}px`;
-    circle.style.top = `${e.pageY}px`;
-  }
-});
+
